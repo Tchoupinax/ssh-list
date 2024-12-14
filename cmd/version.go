23fc30edd9d5357c5bbbc689ffab5a07f996b857
Tchoupinax <corentinfiloche@hotmail.fr>
2024-12-14 23:52:06 +0100

    feat: redesign version display (#8)

--- cmd/version.go
+++ cmd/version.go
@@ -2,17 +2,33 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 )
+
+const Version string = "0.2.0"
+const BuildDate string = "2024-12-14"
 
 func init() {
 	var versionCmd = &cobra.Command{
 		Use:   "version",
-		Short: "Print the version number of Hugo",
-		Long:  `All software has versions. This is Hugo's`,
+		Short: "Print the version number of ssh-list",
+		Long:  "Print the version number of ssh-list",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("v0.0.17")
+			bold := color.New(color.Bold).SprintFunc()
+			italic := color.New(color.Italic).SprintFunc()
+
+			fmt.Println()
+			fmt.Println(bold("⚡️ SSH List"))
+			fmt.Println()
+			fmt.Println("build date: ", bold(BuildDate))
+			fmt.Println("version:         ", bold(Version))
+			fmt.Println()
+			fmt.Println(italic("Need help?"))
+			fmt.Println(italic("https://github.com/Tchoupinax/ssh-list/issues"))
+			os.Exit(0)
 		},
 	}
 
